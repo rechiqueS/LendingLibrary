97ee75b46b4140871fb018663af31170171a6595
Rechique Sukdeo <Rechique.Sukdeo@chillisoft.co.za>
2016-01-15 16:01:51 +0200

    Stock standard application MVC with nUnit Test

---
+++ source/LendingLibrary.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using LendingLibrary.Controllers;
+using NUnit.Framework;
+
+namespace LendingLibrary.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
