11d5cfe7516ebb30b67ac8ef3235dc7ab4c06d54
Rechique Sukdeo <Rechique.Sukdeo@chillisoft.co.za>
2016-02-01 18:21:22 +0200

    Added Domain Projects with Domain Test and tested if the class has properties

---
+++ source/LendingLibrary.Domain.Tests/TestLoan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+
+namespace LendingLibrary.Domain.Tests
+{
+    [TestFixture]
+    public class TestLoan
+    {
+
+// created a TestCase to test Entity class Loan with properties Id & Name.
+        [TestCase("Id", typeof(int))]
+        [TestCase("Name",typeof(string))]
+        public void Type_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var loan = typeof(Loan);
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            loan.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+
+        }
+    }
+}
---
+++ source/LendingLibrary.Domain/Loan.cs
@@ -0,0 +1,8 @@
+﻿namespace LendingLibrary.Domain
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
