56e7e1f5c9616e4646abf735d92f5880d981fabf
Rechique Sukdeo <Rechique.Sukdeo@chillisoft.co.za>
2016-02-01 17:00:28 +0200

    Added the DeleteView

---
+++ source/LendingLibrary/Models/DeleteItemModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LendingLibrary.Models
+{
+    public class DeleteItemModel
+    {
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+    }
+}
