8a52f69205cab0bd84b7e28dc7a07033ebb7fa94
Rechique Sukdeo <Rechique.Sukdeo@chillisoft.co.za>
2016-02-08 16:45:48 +0200

    TDD Loan.cs properties to be Required and have a MaxLength

--- source/LendingLibrary.Domain.Tests/TestLoan.cs
+++ source/LendingLibrary.Domain.Tests/TestLoan.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using PeanutButter.TestUtils.Entity;
 using PeanutButter.TestUtils.Generic;
 
 namespace LendingLibrary.Domain.Tests
@@ -14,17 +15,62 @@ namespace LendingLibrary.Domain.Tests
 
 // created a TestCase to test Entity class Loan with properties Id & Name.
         [TestCase("Id", typeof(int))]
-        [TestCase("Name",typeof(string))]
+        [TestCase("BorrowerName", typeof(string))]
+        [TestCase("ItemDescription", typeof(string))]
         public void Type_ShouldHaveProperty(string propertyName, Type propertyType)
         {
             //---------------Set up test pack-------------------
             var loan = typeof(Loan);
             //---------------Assert Precondition----------------
-
             //---------------Execute Test ----------------------
             loan.ShouldHaveProperty(propertyName, propertyType);
             //---------------Test Result -----------------------
+        }
 
+        [Test]
+        public void BorrowerName_ShouldBeRequired()
+        {
+            //---------------Set up test pack-------------------
+            var loan = new Loan();  
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            loan.ShouldBeRequired(l => l.BorrowerName);
+            //---------------Test Result -----------------------
+           
+        }
+
+        [Test]
+        public void ItemDescription_ShouldRequired()
+        {
+            //---------------Set up test pack-------------------
+            var loan = new Loan();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            loan.ShouldBeRequired(l => l.ItemDescription);
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void BorrowerName_ShouldHaveMaxLenght_100()
+        {
+            //---------------Set up test pack-------------------
+            var loan = new Loan();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            loan.ShouldHaveMaxLengthOf(100, l => l.BorrowerName);
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void ItemDescription_ShouldHaveMaxLength_200()
+        {
+            //---------------Set up test pack-------------------
+            var loan = new Loan();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            loan.ShouldHaveMaxLengthOf(200, l => l.ItemDescription);
+            //---------------Test Result -----------------------
+            
         }
     }
 }
--- source/LendingLibrary.Domain/Loan.cs
+++ source/LendingLibrary.Domain/Loan.cs
@@ -1,8 +1,15 @@
-﻿namespace LendingLibrary.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LendingLibrary.Domain
 {
     public class Loan
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string BorrowerName { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string ItemDescription { get; set; }
     }
 }
